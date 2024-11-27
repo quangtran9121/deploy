1334bbe3c6a61821640c24004b6728c173e4490b
Quang TT <tranquangntch@gmail.com>
2024-11-27 09:47:19 +0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ io.on("connection", (socket) => {
 
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://localhost:9121', // Địa chỉ client
+  origin: 'http://45.77.32.24:9121', // Địa chỉ client
   credentials: true, // Cho phép gửi cookie
 };
 app.use(cors(corsOptions));
