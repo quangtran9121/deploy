55a28f9f760ef52ff9f14a3fe54e351ef048fc5f
Quang TT <tranquangntch@gmail.com>
2024-11-29 11:30:16 +0700

    multi cors nginx 11:30

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const { handleChatMessage } = require('./Controllers/chatbot/ControllerChatbot.j
 // Đọc chứng chỉ SSL từ file (bạn cần có chứng chỉ SSL)
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/fe2.backendintern.online/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/fe2.backendintern.online/fullchain.pem', 'utf8');
-const ca = fs.readFileSync('/path/to/your/ca.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/fe2.backendintern.online/chain.pem', 'utf8');
 
 const credentials = { key: privateKey, cert: certificate, ca: ca };
 
