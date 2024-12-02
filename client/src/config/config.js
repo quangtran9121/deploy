c8fa455d0c6641ba57def6f48d2514d204bfa47d
Quang TT <tranquangntch@gmail.com>
2024-12-02 09:20:42 +0700

    undo axios

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const request = axios.create({
     withCredentials: true,
-    baseURL: 'https://quangtt.backendintern.online',
+    baseURL: 'http://quangtt.backendintern.online',
 });
 
 export default request;
